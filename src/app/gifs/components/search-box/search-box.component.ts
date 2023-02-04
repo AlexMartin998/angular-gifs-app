759643126d36aaff76a47df1cf87e59123c4e562
Alex Martin <alextravc.342@gmail.com>
2023-02-04 16:18:24 -0500

    [ADD]: gifs component added

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Search:</h5>
+    <input type="text" name="search" placeholder="Search gifs..." />
+  `,
+})
+export class SearchBoxComponent {
+  //
+}
