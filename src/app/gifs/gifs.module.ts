1affa1d644d165fbba535308797e33374b3921e6
Alex Martin <alextravc.342@gmail.com>
2023-02-04 21:52:07 -0500

    [ADD]: lazyImage component added

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifsCardComponent } from './components/card/card.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
@@ -14,7 +16,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
     GifsCardComponent,
   ],
 
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
 
   exports: [HomePageComponent],
 })
